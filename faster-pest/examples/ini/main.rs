c99e1647f895581928b5cc3f4ebe6157472fb795
Mubelotix <mubelotix@gmail.com>
2023-01-11 14:29:21 +0100

    Add ini example

---
+++ faster-pest/examples/ini/main.rs
@@ -0,0 +1,48 @@
+use faster_pest::*;
+use std::collections::HashMap;
+
+#[derive(Parser)]
+#[grammar = "faster-pest/examples/ini/ini.pest"]
+pub struct INIParser;
+
+fn main() {
+    let unparsed_file = match std::fs::read_to_string("faster-pest/examples/ini/input.ini") {
+        Ok(s) => s,
+        Err(_) => match std::fs::read_to_string("examples/ini/input.ini") {
+            Ok(s) => s,
+            Err(e) => panic!("cannot read file: {}", e)
+        }
+    };
+
+    let file = INIParser::parse(Rule::file, &unparsed_file)
+        .expect("unsuccessful parse")
+        .next().unwrap(); // get and unwrap the `file` rule; never fails
+
+    let mut properties: HashMap<&str, HashMap<&str, &str>> = HashMap::new();
+
+    let mut current_section_name = "";
+
+    for line in file.into_inner() {
+        match line.as_rule() {
+            Rule::section => {
+                let mut inner_rules = line.into_inner(); // { name }
+                current_section_name = inner_rules.next().unwrap().as_str();
+            }
+            Rule::property => {
+                let mut inner_rules = line.into_inner(); // { name ~ "=" ~ value }
+
+                let name: &str = inner_rules.next().unwrap().as_str();
+                let value: &str = inner_rules.next().unwrap().as_str();
+
+                // Insert an empty inner hash map if the outer hash map hasn't
+                // seen this section name before.
+                let section = properties.entry(current_section_name).or_default();
+                section.insert(name, value);
+            }
+            //Rule::EOI => (),
+            _ => unreachable!(),
+        }
+    }
+
+    println!("{:#?}", properties);
+}
--- faster-pest/src/pairs.rs
+++ faster-pest/src/pairs.rs
@@ -3,7 +3,7 @@ use std::rc::Rc;
 use pest::*;
 
 pub trait IdentTrait: Copy {
-    type Rule: Copy;
+    type Rule: pest::RuleType;
 
     fn as_rule(&self) -> Self::Rule;
     fn as_str(&self) -> &str;
@@ -15,16 +15,16 @@ pub struct Tokens2 {
 }
 
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Pair2<'i, I: IdentTrait> {
     all_idents: Rc<Vec<I>>,
-    ident_range: std::ops::Range<usize>,
+    range: std::ops::Range<usize>,
     initial_text: &'i str,
 }
 
 impl<'i, I: IdentTrait> Pair2<'i, I> {
     pub(crate) fn ident(&self) -> &I {
-        self.all_idents.get(self.ident_range.start).unwrap()
+        self.all_idents.get(self.range.start).unwrap()
     }
 
     pub fn as_rule(&self) -> I::Rule {
@@ -48,25 +48,38 @@ impl<'i, I: IdentTrait> Pair2<'i, I> {
         Span::new(self.initial_text, start, end).unwrap()
     }
 
-    pub fn into_inner(self) -> Pairs2<'i, I> {
+    pub fn inner(&self) -> Pairs2<'i, I> {
         Pairs2 {
             all_idents: Rc::clone(&self.all_idents),
-            ident_range: self.ident_range.start + 1..self.ident_range.end,
+            range: self.range.start + 1..self.range.end,
             initial_text: self.initial_text,
             i: 0,
         }
     }
+
+    pub fn into_inner(self) -> Pairs2<'i, I> { self.inner() }
 
     pub fn tokens(self) -> Tokens2 {
         unimplemented!()
     }
 }
 
+impl<'i, I: IdentTrait> std::fmt::Debug for Pair2<'i, I> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Pair2")
+            .field("rule", &self.as_rule())
+            .field("text", &self.as_str())
+            .field("range", &self.range)
+            .field("inner", &self.inner())
+            .finish()
+    }
+}
 
-#[derive(Debug, Clone)]
+
+#[derive(Clone)]
 pub struct Pairs2<'i, I: IdentTrait> {
     all_idents: Rc<Vec<I>>,
-    ident_range: std::ops::Range<usize>,
+    range: std::ops::Range<usize>,
     initial_text: &'i str,
     i: usize,
 }
@@ -74,7 +87,7 @@ pub struct Pairs2<'i, I: IdentTrait> {
 impl<'i, I: IdentTrait> Pairs2<'i, I> {
     pub fn from_idents(idents: Vec<I>, initial_text: &'i str) -> Self {
         Self {
-            ident_range: 0..idents.len(),
+            range: 0..idents.len(),
             all_idents: Rc::new(idents),
             initial_text,
             i: 0,
@@ -86,28 +99,38 @@ impl<'i, I: IdentTrait + 'i> Iterator for Pairs2<'i, I> {
     type Item = Pair2<'i, I>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if (self.i + self.ident_range.start) >= self.all_idents.len() || self.i >= self.ident_range.end {
+        if (self.i + self.range.start) >= self.all_idents.len() || self.i >= self.range.len() {
             return None;
         }
-        let i = self.i + self.ident_range.start;
+        let i = self.i + self.range.start;
 
         let inner_end = self.all_idents[i].as_str().as_ptr() as usize + self.all_idents[i].as_str().len() - self.initial_text.as_ptr() as usize;
         let start = i;
         let mut end = i + 1;
         while let Some(ident) = self.all_idents.get(end) {
             let ident_start = ident.as_str().as_ptr() as usize - self.initial_text.as_ptr() as usize;
-            if ident_start >= inner_end {
+            if ident_start >= inner_end && (ident_start + ident.as_str().len()) > inner_end {
                 break;
             }
             end += 1;
         }
 
-        self.i = end - self.ident_range.start;
+        self.i = end - self.range.start;
 
         Some(Pair2 {
             all_idents: Rc::clone(&self.all_idents),
             initial_text: self.initial_text,
-            ident_range: start..end,
+            range: start..end,
         })
+    }
+}
+
+impl<'i, I: IdentTrait> std::fmt::Debug for Pairs2<'i, I> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries({
+            let mut clone = self.clone();
+            clone.i = 0;
+            clone
+        }).finish()
     }
 }
