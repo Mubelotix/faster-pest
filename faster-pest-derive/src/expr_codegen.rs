b105e5da21f7c18bd6920e9eb16908379fe0a054
Mubelotix <mubelotix@gmail.com>
2023-01-11 21:44:40 +0100

    Optimization

--- faster-pest-derive/src/expr_codegen.rs
+++ faster-pest-derive/src/expr_codegen.rs
@@ -285,6 +285,56 @@ pub fn code(expr: &OptimizedExpr, ids: &mut IdRegistry, has_whitespace: bool) ->
             "#)
         }
         OptimizedExpr::Rep(expr) => {
+            // If we loop over a character selector, optimize this away
+            if matches!(expr.as_ref(), OptimizedExpr::Choice(_, _)) {
+                let mut choices = Vec::new();
+                list_choices(expr, &mut choices);
+                let mut simple_conditions = Vec::new();
+                for choice in &choices {
+                    match choice {
+                        OptimizedExpr::Str(s) if s.len() == 1 => simple_conditions.push(format!("c == &b'{s}'")),
+                        OptimizedExpr::Ident(i) => if let Some((_, c)) = CONDITIONS.iter().find(|(n,_)| n == i) {
+                            simple_conditions.push(c.to_string());
+                        }
+                        _ => break,
+                    }
+                }
+                if simple_conditions.len() == choices.len() {
+                    let condition = simple_conditions.join(" || ");
+                    return format!(r#"
+                    // {hr_expr}
+                    fn parse_{id}<'i, 'b>(mut input: &'i str, {formatted_idents}) -> Result<&'i str, Error> {{
+                        let b = input.as_bytes();
+                        let mut i = 0;
+                        while i < b.len() {{
+                            let c = unsafe {{ b.get_unchecked(i) }};
+                            if {condition} {{
+                                i += 1;
+                            }} else {{
+                                break;
+                            }}
+                        }}
+                        Ok(unsafe {{ input.get_unchecked(i..) }})
+                    }}
+                    fn quick_parse_{id}<'i, 'b>(mut input: &'i str, {formatted_idents}) -> Option<&'i str> {{
+                        let b = input.as_bytes();
+                        let mut i = 0;
+                        while i < b.len() {{
+                            let c = unsafe {{ b.get_unchecked(i) }};
+                            if {condition} {{
+                                i += 1;
+                            }} else {{
+                                break;
+                            }}
+                        }}
+                        Some(unsafe {{ input.get_unchecked(i..) }})
+                    }}
+        
+                    "#);
+        
+                }
+            }
+
             let expr_id = ids.id(expr);
             let idents = match contains_idents(expr, has_whitespace) {
                 true => "idents",
