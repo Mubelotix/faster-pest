eab1cf332765b5757641c507c9d0f16c6212ba9e
Mubelotix <mubelotix@gmail.com>
2023-07-01 01:29:25 +0200

    Update pest

--- faster-pest-derive/src/optimizer.rs
+++ faster-pest-derive/src/optimizer.rs
@@ -118,7 +118,6 @@ pub fn optimize(expr: &OptimizedExpr) -> FPestExpr {
                 todo!()
             }
         }
-        OptimizedExpr::NodeTag(expr, _) => optimize(expr),
         OptimizedExpr::PosPred(_) => todo!(),
         OptimizedExpr::Skip(_) => todo!(),
         OptimizedExpr::Push(_) => todo!(),
