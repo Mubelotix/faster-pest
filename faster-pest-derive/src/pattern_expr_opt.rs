1e23bafcb483aec8e3e5205ef7604be6106bd9b1
Mubelotix <mubelotix@gmail.com>
2024-05-07 16:19:30 +0200

    Fix opt pattern

--- faster-pest-derive/src/pattern_expr_opt.rs
+++ faster-pest-derive/src/pattern_expr_opt.rs
@@ -3,22 +3,23 @@ pub fn parse_expr_id<'i, 'b>(
     input: &'i [u8],
     //SIG-IDENTS idents: &'b mut Vec<(Ident<'i>, usize)>
 ) -> Result<&'i [u8], Error> {
-    let idents_len = idents.len();
-    if let Ok(input) = parse_inner_eid(input, {idents}) {
+    //SIG-IDENTS let idents_len = idents.len();
+    if let Ok(input) = parse_inner_eid(input, inner_idents) {
         Ok(input)
     } else {
-        unsafe { idents.set_len(idents_len); }
+        //SIG-IDENTS unsafe { idents.set_len(idents_len); }
         Ok(input)
     }
 }
 pub fn quick_parse_expr_id<'i, 'b>(
     input: &'i [u8],
     //SIG-IDENTS idents: &'b mut Vec<(Ident<'i>, usize)>
-) -> Option<&'i [u8]> {    let idents_len = idents.len();
+) -> Option<&'i [u8]> {
+    //SIG-IDENTS let idents_len = idents.len();
     if let Some(input) = quick_parse_inner_eid(input, inner_idents) {
         Some(input)
     } else {
-        unsafe { idents.set_len(idents_len); }
+        //SIG-IDENTS unsafe { idents.set_len(idents_len); }
         Some(input)
     }
 }
